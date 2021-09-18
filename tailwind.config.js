d18e48d6010073651a332555bf8c760d9a2b8cb5
David Colby <davidlcolby@gmail.com>
2021-09-17 22:34:57 -0500

    Initial commit, Tailwind installed, Teams and Players created

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [
+    './app/**/*/*.html.erb',
+    './app/helpers/**/*/*.rb',
+    './app/javascript/**/*/*.js',
+    './app/javascript/**/*/*.vue',
+    './app/javascript/**/*/*.react'
+  ],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
